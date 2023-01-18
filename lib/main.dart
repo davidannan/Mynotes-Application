46b99aeb0b42a2521cf1d6abac35cff92ea90874
davidannan <annand556@gmail.com>
2023-01-18 16:12:43 +0000

    step 3.1

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'views/verify_email_view.dart';
-import 'dart:developer' as devtools show log;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,8 +17,10 @@ void main() {
     routes: {
       '/login/': (context) => const LoginView(),
       '/register/':(context) => const RegisterView(),
+      '/notes/': (context) => const NotesView(),
     },
-  ));
+  ),
+  );
 }
 
 class HomePage extends StatelessWidget {
