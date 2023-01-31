0ccf723d3ba491c38382ba64a2fad04f87d32a91
davidannan <annand556@gmail.com>
2023-01-31 20:24:52 +0000

    database function 1

--- lib/services/crud/notes_services.dart
+++ lib/services/crud/notes_services.dart
@@ -9,43 +9,84 @@ class UnableToGetDocumentsDirectory implements Exception {}
 
 class DatabaseIsNotOpen implements Exception {}
 
-class NotesService{
+class CouldNotDeleteUser implements Exception {}
+
+class UserAlreadyExists implements Exception {}
+
+class NotesService {
   Database? _db;
 
-Future<void> close() async {
-  final db =_db;
-  if (db == null) {
-    throw DatabaseIsNotOpen();
-  }else {
-    await db.close();
-    _db = null;
+  Future<DatabaseUser> createUser({required String email}) async {
+    final db = _getDatabaseOrThrow();
+    final results = await db.query(
+      userTable,
+      limit: 1,
+      where: 'email = ?',
+      whereArgs: [email.toLowerCase()],
+    );
+    if (results.isNotEmpty) {
+      throw UserAlreadyExists();
+    }
+    final userId = await db.insert(userTable, {
+      emailColumn: email.toLowerCase(),
+    });
+    return DatabaseUser(
+      id: userId,
+      email: email,
+    );
   }
-}
+
+  Future<void> deleteUser({required String email}) async {
+    final db = _getDatabaseOrThrow();
+    final deletedCount = await db.delete(
+      userTable,
+      where: 'email = ?',
+      whereArgs: [email.toLowerCase()],
+    );
+    if (deletedCount != 1) {
+      throw CouldNotDeleteUser();
+    }
+  }
+
+  Database _getDatabaseOrThrow() {
+    final db = _db;
+    if (db == null) {
+      throw DatabaseIsNotOpen();
+    } else {
+      return db;
+    }
+  }
+
+  Future<void> close() async {
+    final db = _db;
+    if (db == null) {
+      throw DatabaseIsNotOpen();
+    } else {
+      await db.close();
+      _db = null;
+    }
+  }
 
   Future<void> open() async {
-    if (_db != null){
+    if (_db != null) {
       throw DatabaseAlreadyOpenException();
     }
-    try{
+    try {
       final docsPath = await getApplicationDocumentsDirectory();
       final dbPath = join(docsPath.path, dbName);
       final db = await openDatabase(dbPath);
       _db = db;
 
+      // create the user table
+      await db.execute(createUserTable);
+      // create note table
 
-    // create the user table
-await db.execute(createUserTable);
-    // create note table
-
-
-await db.execute(createNotesTable);
+      await db.execute(createNotesTable);
     } on MissingPlatformDirectoryException {
       throw UnableToGetDocumentsDirectory();
     }
   }
 }
-
-
 
 @immutable
 class DatabaseUser {
@@ -88,14 +129,14 @@ class DatabaseNote {
         userId = map[userIdColumn] as int,
         text = map[textColumn] as String,
         isSyncedWithCloud =
-            (map[isSyncedWithCloudColumn] as int) ==1 ? true : false;
+            (map[isSyncedWithCloudColumn] as int) == 1 ? true : false;
 
-@override
-String toString() =>
-'Note, ID =$id, userId =$userId, isSyncedWithCloud = $isSyncedWithCloud, text = $text';
+  @override
+  String toString() =>
+      'Note, ID =$id, userId =$userId, isSyncedWithCloud = $isSyncedWithCloud, text = $text';
 
-@override
-bool operator ==(covariant DatabaseNote other) => id == other.id;
+  @override
+  bool operator ==(covariant DatabaseNote other) => id == other.id;
 
   @override
   int get hashCode => id.hashCode;
