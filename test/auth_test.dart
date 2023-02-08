979aac241293d9c0584fd3482710c5541b9a1e4a
davidannan <annand556@gmail.com>
2023-02-08 20:34:14 +0000

    user interface functionality

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -112,7 +112,7 @@ class MockAuthProvider implements AuthProvider {
     await Future.delayed(const Duration(seconds: 1));
     if (email == 'kojo@adent.com') throw UserNotFoundAuthException();
     if (password == 'annan') throw WrongPasswordAuthException();
-    const user = AuthUser(isEmailVerified: false, email: '');
+    const user = AuthUser(isEmailVerified: false, email: 'foo@bar.com');
     _user = user;
     return Future.value(user);
   }
@@ -130,7 +130,7 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedEException();
     final user = _user;
     if (user == null) throw UserNotFoundAuthException();
-    const newUser = AuthUser(isEmailVerified: true, email: '');
+    const newUser = AuthUser(isEmailVerified: true, email: 'foo@bar.com');
     _user = newUser;
   }
 }
