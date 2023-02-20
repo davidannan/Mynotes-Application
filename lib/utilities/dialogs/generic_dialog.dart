8e39863f53bfd337c6249949092508a7d3e8c23e
davidannan <annand556@gmail.com>
2023-02-20 17:15:29 +0000

    Generic Dialog step 2

--- lib/utilities/dialogs/generic_dialog.dart
+++ lib/utilities/dialogs/generic_dialog.dart
@@ -5,22 +5,27 @@ import 'package:flutter/material.dart';
 typedef DialogOptionBuilder<T> = Map<String, T?> Function();
 
 Future<T?> showGenericDialog<T>({
-  required BuildContext contex,
+  required BuildContext context,
   required String title,
   required String content,
   required DialogOptionBuilder optionsBuilder,
 }){
  final options = optionsBuilder();
  return showDialog<T>(
-  context: contex,
+  context: context,
   builder: (contex) {
     return AlertDialog(
       title: Text(title),
       content: Text(content),
       actions:options.keys.map((optionTitle) {
         final T value = options[optionTitle];
-        return TextButton(onPressed: () {
-
+        return TextButton(
+          onPressed: () {
+            if (value != null) {
+              Navigator.of(context).pop(value);
+            } else {
+              Navigator.of(context).pop();
+            }
         }, child: Text (optionTitle)
         );
       }).toList(),
