752a1ef896f1cc25f3898ce73b495a53292abb0d
davidannan <annand556@gmail.com>
2023-03-01 19:47:34 +0000

    Fixing Notes View 8

--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -6,7 +6,7 @@ class VerifyEmailView extends StatefulWidget {
   const VerifyEmailView({Key? key}) : super(key: key);
 
   @override
-  _VerifyEmailViewState createState() => _VerifyEmailViewState();
+State createState() => _VerifyEmailViewState();
 }
 
 class _VerifyEmailViewState extends State<VerifyEmailView> {
