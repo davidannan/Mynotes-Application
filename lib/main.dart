bcf4ba0542cd9dac857f796e5e2a86e3fd8d37b2
davidannan <annand556@gmail.com>
2023-03-04 21:31:11 +0000

    tracking errors

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
     MaterialApp(
-    title: 'My NOTES App',
+    title: 'My NOTES',
     theme: ThemeData(
       primarySwatch: Colors.blue,
     ),
